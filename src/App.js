878ee8a2ef6ed6b9dc761198a764f8fae833ba04
minasyannat <minasyannatalie5@gmail.com>
2023-03-29 15:58:34 +0400

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import {TextField} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      Alias by Natalie
+      <Header />
+      <TextField id="standard-basic" label="Team 1 name" variant="standard" />
+      <TextField id="standard-basic" label="Team 2 name" variant="standard" />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+
+    return (
+        <div className="header">
+            <h1>Alias</h1>
+            <img src="https://cdn.pixabay.com/photo/2015/12/23/17/58/question-mark-1105892_960_720.jpg"></img>
+        </div>
+    )
+}
+
+export default Header;
